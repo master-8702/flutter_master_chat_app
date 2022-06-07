e94ca0282321c224ef377857aab83236341f87dc
master-8702 <55705389+master-8702@users.noreply.github.com>
2022-06-07 21:51:20 +0300

    firebase user signing out Done

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -39,9 +39,10 @@ class _ChatScreenState extends State<ChatScreen> {
         leading: null,
         actions: <Widget>[
           IconButton(
-              icon: Icon(Icons.close),
+              icon: Icon(Icons.logout_outlined),
               onPressed: () {
-                //Implement logout functionality
+                _auth.signOut();
+                Navigator.pop(context);
               }),
         ],
         title: Text('M️Chat'),
